b8a9b8028dead1b4cd659c1fcda1cab142a952b9
OnelKND <angekounde3@gmail.com>
2026-07-03 16:22:39 +0100

    fix: remove unused imports in Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,4 +1,4 @@
-import { FolderCode, Github, Linkedin, Twitter, Mail, Facebook } from "lucide-react";
+import { FolderCode, Github, Mail, Facebook } from "lucide-react";
 
 const Footer = () => {
   const currentYear = new Date().getFullYear();
